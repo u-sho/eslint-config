a05251cc03c84ab738fa199222f393216e3981ac
u-sho <26079835+u-sho@users.noreply.github.com>
2025-06-15 13:33:54 +0900

    fix: editor settings

--- configs/all-ts.js
+++ configs/all-ts.js
@@ -8,14 +8,41 @@ import getConfigNode    from '../plugin-configs/eslint-plugin-n/all.js';
 import getConfigPromise from '../plugin-configs/eslint-plugin-promise/all.js';
 
 import getConfigEslintComments from '../plugin-configs/@eslint-community/eslint-plugin-eslint-comments/format.js';
-import getConfigStylistic      from '../plugin-configs/@stylistic/format.js';
+import getConfigStylistic from '../plugin-configs/@stylistic/format.js';
 
 
 /**
+ * @typedef {import('@typescript-eslint/utils/ts-eslint').FlatConfig.Config} Config
+ * @returns {Omit<Config, 'files'> & {files: string[]}}
+ *
  * @param {import('eslint').Linter.RuleSeverity} [logLevel='error']
  * @param {import('eslint').Linter.RuleSeverity} [formatLogLevel='warn']
- * @param {import('./base.js').JsConfigOptions & {readonly isJsFile?: boolean}} [option={}]
- * @returns {import('@typescript-eslint/utils/ts-eslint').FlatConfig.Config}
+ * @param {import('./base.js').JsConfigOptions & {readonly isJsFile?: boolean}} [option={}] default:
+ * ```ts
+ * {
+ *   complexityDepth: Infinity,
+ *   printWidth     : 100,
+ *   tabWidth       : 3,
+ *   short          : false,
+ *   arrowParens    : false,
+ *   blockSpacing   : true,
+ *   braceStyle     : '1tbs',
+ *   commaDangle    : 'never',
+ *   indent         : 'tab',
+ *   jsx            : false,
+ *   quoteProps     : 'consistent-as-needed',
+ *   quotes         : 'single',
+ *   semi           : true,
+ *   isJsFile       : false,
+ *
+ *   eslintCommentsPluginName: '@eslint-community/eslint-comments',
+ *             nodePluginName: 'n',
+ *          promisePluginName: 'promise',
+ *            reactPluginName: 'react',
+ *        stylisticPluginName: '@stylistic',
+ *               tsPluginName: '@typescript-eslint'
+ * }
+ * ```
  */
 export const getConfigTsAll = (/* eslint-disable @stylistic/indent */
 	      logLevel = 'error',
@@ -42,8 +69,8 @@ export const getConfigTsAll = (/* eslint-disable @stylistic/indent */
 		       promisePluginName = 'promise',
 		         reactPluginName = 'react',
 		     stylisticPluginName = '@stylistic',
-		            tsPluginName = '@typescript-eslint' /* eslint-enable @stylistic/indent */
-	} = {}
+		            tsPluginName = '@typescript-eslint'
+	} = {} /* eslint-enable @stylistic/indent */
 ) => {
 	const configJs = getConfigJs(logLevel, formatLogLevel, {complexityDepth, jsx, semi});
 	const configTs = getConfigTs(logLevel, formatLogLevel, {short, javascript: isJsFile, pluginName: tsPluginName});
